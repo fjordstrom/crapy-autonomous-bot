be4bdea9cafa4bef6da7b2c008afe05da05e8eae
Valentin Fleșeriu <fjordstrom@gmail.com>
2016-03-22 05:35:32 +0200

    Add ORB test

---
+++ computer_vision/test_ORB.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+
+import MockCameraHandler as ch
+
+ch.initFrames()
+
+ch.grabFrames()
+img = ch.imageLeft
+
+# Initiate STAR detector
+orb = cv2.ORB()
+
+# find the keypoints with ORB
+kp = orb.detect(img,None)
+
+# compute the descriptors with ORB
+kp, des = orb.compute(img, kp)
+
+# draw only keypoints location,not size and orientation
+img2 = cv2.drawKeypoints(img,kp,color=(0,255,0), flags=0)
+
+plt.imshow(img2),plt.show()
